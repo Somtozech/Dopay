af0a070fc94da48eb6033dfdb19f3c84fcb9e02e
Somtozech <ezechinnaemeka@gmail.com>
2022-02-28 23:19:50 +0100

    feat: Setup Oracle Contract

---
+++ contracts/Oracle.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Oracle is Ownable {
+    uint256 public constant EXPIRY_TIME = 5 minutes;
+    uint256 private randomNonce = 0;
+    mapping(uint256 => bytes32) pendingRequests;
+
+    event OracleRequest(uint256 indexed id, address sender, uint256 expiration);
+
+    function oracleRequest(address _sender) public {
+        randomNonce++;
+
+        // generate random number
+        uint256 id = uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, msg.sender, randomNonce)
+            )
+        ) % 1000;
+        uint256 expiration = block.timestamp + EXPIRY_TIME;
+        bytes32 hash = keccak256(abi.encodePacked(id, _sender, expiration));
+        pendingRequests[id] = hash;
+
+        emit OracleRequest(id, _sender, expiration);
+    }
+
+    function fulfillOracleRequest(
+        uint256 _id,
+        address _sender,
+        uint256 _expiration,
+        bytes32 _data
+    ) public onlyOwner {
+        bytes32 hash = keccak256(abi.encodePacked(_id, _sender, _expiration));
+
+        require(hash == pendingRequests[_id], "Params dosen't match");
+        delete pendingRequests[_id];
+
+        __callback(_data);
+    }
+
+    function __callback(bytes32 _data) public virtual {}
+}
